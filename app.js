309b4800c67f05bc260ec6236ea35caf70fba518
Muhammad Fuadi <fuadevo19@gmail.com>
2024-03-28 15:26:02 +0700

    Membuat port ke 8000

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const fs = require('fs');
-
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
-let datah = JSON.parse(fs.readFileSync('./data/datah.json'));
+let datah = JSON.parse(fs.readFileSync("./data/datah.json"));
 
 // Definisikan rute dan aturan aplikasi Express di sini
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 //GET - API
-app.get('/api/v1/datah', (req, res) => {
-    res.status(200).json({
-        status: "sucess",
-        count: datah.length,
-        data: {
-            datah: datah
-        }
-    });
+app.get("/api/v1/datah", (req, res) => {
+  res.status(200).json({
+    status: "sucess",
+    count: datah.length,
+    data: {
+      datah: datah,
+    },
+  });
 });
 
-app.get('/api/v1/datah/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const selectedData = datah.find(item => item.id === id);
+app.get("/api/v1/datah/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedData = datah.find((item) => item.id === id);
 
-    if (!selectedData) {
-        return res.status(404).json({
-            status: "error",
-            message: "Data not found"
-        });
-    }
-
-    res.status(200).json({
-        status: "success",
-        data: {
-            datah: selectedData
-        }
+  if (!selectedData) {
+    return res.status(404).json({
+      status: "error",
+      message: "Data not found",
     });
-});
+  }
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+  res.status(200).json({
+    status: "success",
+    data: {
+      datah: selectedData,
+    },
+  });
+});
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000");
 });
