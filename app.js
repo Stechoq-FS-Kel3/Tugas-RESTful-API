b417a707cc669a5286b779adc72ee89163b7b5d1
Juwandito <juwanditorajendrawrahatnolo@gmail.com>
2024-03-29 03:55:53 +0700

    POST API

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let datah = JSON.parse(fs.readFileSync("./data/datah.json"));
 
 // Definisikan rute dan aturan aplikasi Express di sini
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -19,6 +21,25 @@ app.get("/api/v1/datah", (req, res) => {
     },
   });
 });
+
+//POST - API
+app.post('/Tugas-RESTful-API/datah.json', (req,res)=> {
+  // console.log(req.body);
+  const newId = datah[datah.length - 1].id + 1;
+
+  const newdata = Object.assign({id:newId}, req.body)
+  datah.push(newdata);
+
+  fs.writeFile('/Tugas-RESTful-API/datah.json',JSON stringify(datah), (err) => {
+    res.status(201).json({
+      status: "succes",
+      data: {
+        datah: newdata
+      }
+    })
+  }
+  // res.send('Created');
+})
 
 app.get("/api/v1/datah/:id", (req, res) => {
   const id = parseInt(req.params.id);
